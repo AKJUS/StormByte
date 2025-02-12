6f7fb5ead29dd4466a69146a441f166f486bae6a
David C. Manuelda <StormByte@gmail.com>
2025-02-12 15:56:42 +0100

    Doxygen doc start

--- StormByte/config/item/value/bool.hxx
+++ StormByte/config/item/value/bool.hxx
@@ -5,6 +5,9 @@
 #include <variant>
 
 namespace StormByte::Config {
+	/**
+	 * Class representing a boolean in configuration files (true or false in lowercase)
+	 */
 	class STORMBYTE_PUBLIC Bool final: public Value {
 		public:
 			Bool(const std::string&);
@@ -15,11 +18,23 @@ namespace StormByte::Config {
 			Bool& operator=(Bool&&) noexcept	= default;
 			~Bool() noexcept override			= default;
 
+			/**
+			 * Gets the boolean item value
+			 * @return boolean value
+			 */
 			bool 					AsBool() const override;
 
-			void					SetBool(bool) override;
+			/**
+			 * Sets the boolean item value
+			 * @param value boolean value
+			 */
+			void					SetBool(bool value) override;
 
-			std::string				Serialize(const int&) const noexcept override;
+			/**
+			 * Serializes the boolean item:
+			 * @return Serialized string, for example: item = false;
+			 */
+			std::string				Serialize(const int& indent) const noexcept override;
 
 		private:
 			std::shared_ptr<Item>	Clone() override;
